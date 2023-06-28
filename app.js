8c429179c4f41fbaed2ee1c9716d681987cb3929
montaj <sekhmontajulali1997@gmail.com>
2023-06-28 11:54:56 +0530

    classsr44 all file

---
+++ app.js
@@ -0,0 +1,189 @@
+ const davs = [
+    {
+        id:1,
+        name: 'montaj',
+        age: 25,
+        skill: 'js',
+        Location: 'birshibpur',
+        income: 30000,
+        
+
+    },
+    {
+        id:2,
+        name: 'mon',
+        age: 75,
+        skill: 'java',
+        Location: 'birpur',
+        income: 60000,
+
+
+    },
+    {
+        id:3,
+        name: 'maj',
+        age: 85,
+        skill: 'php',
+        Location: 'ibpur',
+        income: 90000,
+
+
+    },
+    {
+        id: 4,
+        name: 'taj',
+        age: 26,
+        skill: 'python',
+        Location: 'shibpur',
+        income: 50000,
+
+
+    },
+    {
+        id:5,
+        name: 'moj',
+        age: 63,
+        skill: 'block_chain',
+        Location: 'pur',
+        income: 130000,
+
+
+    },
+    {
+        id:6,
+        name: 'raj',
+        age: 35,
+        skill: 'mern_srack',
+        Location: 'bpur',
+        income: 120000,
+
+
+    },
+];
+// davs.forEach((item)=>{
+
+//     console.log(`Hi, ${item.name}, you ${item.age}. you are a ${item.skill} develper and your Location ${item.Location}. you get monthly salary ${item.income}. `   )});
+ 
+
+// const studentsResult = 
+//     {
+//         id:1,
+//         name: 'montaj',
+//         age: 15,
+//         sub: {
+//             ban:34,
+//             eng: 32,
+//             math:87,
+//             history: 76,
+//             science:54,
+//             bioScience:64,
+//         },
+//     }
+
+
+// const getvalu = studentsResult.sub
+// console.log(getvalu ['ban']);
+
+// for ( sudv in getvalu) {
+  
+// console.log(getvalu [sudv]);
+// }
+// const total = 0;
+
+// function gpa(params) {
+//   total = total + getvalu [sudv]
+
+// }
+//  console.log(gpa);
+
+
+ 
+
+// const fifthClassResult = (mark) =>{
+
+//     let gpa ;
+//     let grade;
+
+//     if (mark =>0 && mark <33) {
+//         gpa = 0;
+//         grade= 'D';
+        
+//     } else if (mark =>33 && mark <=40) {
+//         gpa = 1;
+//         grade= 'c';
+//     } else if (mark => 41 && mark <=50) {
+//         gpa = 2;
+//         grade= 'c+';
+//     }  else if (mark => 51 && mark <=60) {
+//         gpa = 2.5;
+//         grade= 'c++';
+//     }  else if (mark => 61 && mark <=70) {
+//         gpa = 3;
+//         grade= 'b+';
+//     } else if (mark => 81 && mark <=90) {
+//         gpa = 4;
+//         grade= 'A+';
+//     } else if (mark => 91 && mark <=100) {
+//         gpa = 5;
+//         grade= 'AA+';
+//     }
+
+//    return {
+//     gpa : gpa ,
+//     grade :grade,
+//    }
+// };
+
+
+
+// const student = {
+//     id: 1,
+//     name: "montaj",
+//     age: 15,
+//     sub: {
+//       ban: 34,
+//       eng: 32,
+//       math: 87,
+//       history: 76,
+//       science: 54,
+//       bioScience: 64,
+//       religion: 100,
+//     },
+//   };
+
+//   let subject = student.sub;
+
+
+//   console.log( subject); 
+// for (key in subject){
+// console.log(subject [key]);
+// }
+
+
+
+
+
+
+  
+//   function getCgpa(subs) {
+//     let total = 0;
+//     for (key in subs) {
+//       total = total + subs[key];
+//     }
+//     return total;
+//   }
+  
+//   let totalMark = getCgpa(student.sub);
+  
+//   console.log(getCgpa);
+
+
+
+
+
+
+
+
+
+
+
